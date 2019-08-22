e5134e53ae3e6b5ce434ee07525dbc27bca4b1a2
John C Kallen <John.C.Kallen@aexp.com>
2019-08-22 16:45:50 -0600

    Uncommented Facial Rec

--- src/containers/pages/HomePage.jsx
+++ src/containers/pages/HomePage.jsx
@@ -28,22 +28,22 @@ class HomePage extends Component {
 
   componentDidMount = () => {
   
-    // let videoPlayer = document.querySelector('#player');
-    // let canvasElement = document.querySelector('#canvas');
+    let videoPlayer = document.querySelector('#player');
+    let canvasElement = document.querySelector('#canvas');
 
-    // setTimeout(
-    //   async () => {
-    //    let userFacesData = JSON.parse(localStorage.getItem("userFacesData")) || [];
-    //    let faceUserName = userFacesData[0].user;
-    //    await getFaceMatcher(userFacesData);
+    setTimeout(
+      async () => {
+       let userFacesData = JSON.parse(localStorage.getItem("userFacesData")) || [];
+       let faceUserName = userFacesData[0].user;
+       await getFaceMatcher(userFacesData);
        
-    //    this.setState({
-    //     faceUserName: faceUserName,
-    //    });
-    //    await secureHomePage(videoPlayer,canvasElement,faceUserName);
+       this.setState({
+        faceUserName: faceUserName,
+       });
+       await secureHomePage(videoPlayer,canvasElement,faceUserName);
 
 
-    // },800);
+    },800);
 
 }
 
