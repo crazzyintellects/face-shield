6abce0404b4c42cd9b248e0793b0e8b507131a9b
jsing120 <jasmeet.singh3@aexp.com>
2019-08-22 12:55:50 -0600

    updating DRAWER

--- src/containers/pages/HomePage.jsx
+++ src/containers/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import StatementBalance from '../../components/HeroSection/StatementBalance';
 import Footer from '../../components/Footer/Footer';
+import Drawer from '../../components/Drawer/DrawerFixed'
 
 const headerItems = [
   'Home',
@@ -18,6 +19,7 @@ const HomePage = () => (
  
     <div className="homepage">
       <Header headerItems={headerItems} buttonName={buttonName} />
+      <Drawer />
       <StatementBalance />
       <Footer />    
     </div>
